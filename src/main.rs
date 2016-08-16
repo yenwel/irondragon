59237e52c61555cd2d6906f8ba2efc00e52183b8
Jan Vandepitte <yenwel1987@gmail.com>
2016-08-16 07:16:20 +0000

    router

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
 
 fn main() {
+	let mut router = Router::new();
+	router.get("/",hello_world);
+	router.post("/dragonacceptsriddle",accept_riddle);
 	fn hello_world(_: &mut Request) -> IronResult<Response> {
 		Ok(Response::with((status::Ok, "Hello World!")))
 	}
-	
-	Iron::new(hello_world).http("192.168.0.238:3000").unwrap();
+	fn accept_riddle(_: &mut Request) -> IronResult<Response> {
+		Ok(Response::with((status::Ok, "Riddle accepted!")))
+	}
+	Iron::new(router).http("192.168.0.238:3000").unwrap();
 	println!("On 3000");
 }
